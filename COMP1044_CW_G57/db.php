08192f9bdd61a4f4c9a50194eaef75ec9c2437c6
WANG JINBO <wangjinbo3482385308@gmail.com>
2026-03-17 17:55:27 +0800

    Add database connection script in db.php

---
+++ COMP1044_CW_G57/db.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$username = "root";       
+$password = "root";      
+$dbname = "internship_db";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Invalid connection: " . $conn->connect_error);
+} 
+
+
+echo "Great！Dtabase connecting successfully！";
+?>
