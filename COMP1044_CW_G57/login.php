3c6089af7f463dd728b07302a1a0dfca41e426c6
WANG JINBO <wangjinbo3482385308@gmail.com>
2026-03-24 09:50:12 +0800

    Update login.php

--- COMP1044_CW_G57/login.php
+++ COMP1044_CW_G57/login.php
@@ -38,8 +38,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login - Internship Grade Management System</title>
     <link rel="stylesheet" href="css/style.css">
-    <style>
-    </style>
 </head>
 <body>
     <div class="login-container">
